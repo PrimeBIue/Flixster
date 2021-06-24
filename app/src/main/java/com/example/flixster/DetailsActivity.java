513fe6bc2c22a93ac40bf01dab1ffb6c779e3788
Jorge Cabiedes <jorgecab@fb.com>
2021-06-24 09:21:32 -0500

    Reduced view Boilerplate with ViewBding on DetailsActivity.

--- app/src/main/java/com/example/flixster/DetailsActivity.java
+++ app/src/main/java/com/example/flixster/DetailsActivity.java
@@ -4,33 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.example.flixster.databinding.ActivityDetailsBinding;
+import com.example.flixster.databinding.ActivityMainBinding;
+
 public class DetailsActivity extends AppCompatActivity {
 
-    TextView tvTitleDet;
-    TextView tvOverviewDet;
-    ImageView ivPosterDet;
-    RatingBar rbMovie;
+    ActivityDetailsBinding detailsActivityBinding;
+    
     String imageUrl;
     float rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        detailsActivityBinding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view = detailsActivityBinding.getRoot();
+        setContentView(view);
 
-        tvTitleDet = findViewById(R.id.tvTitleDet);
-        tvOverviewDet = findViewById(R.id.tvOverviewDet);
-        ivPosterDet = findViewById(R.id.ivPosterDet);
-        rbMovie = findViewById(R.id.rbMovie);
+        // setContentView(R.layout.activity_details);
 
         getSupportActionBar().setTitle("Movie Details");
 
-        tvTitleDet.setText(getIntent().getStringExtra(MainActivity.KEY_MOVIE_TITLE));
-        tvOverviewDet.setText(getIntent().getStringExtra(MainActivity.KEY_MOVIE_OVERVIEW));
+        detailsActivityBinding.tvTitleDet.setText(getIntent().getStringExtra(MainActivity.KEY_MOVIE_TITLE));
+        detailsActivityBinding.tvOverviewDet.setText(getIntent().getStringExtra(MainActivity.KEY_MOVIE_OVERVIEW));
         rating = (float) getIntent().getDoubleExtra(MainActivity.KEY_RATING, 0.00)/2;
         imageUrl = getIntent().getStringExtra(MainActivity.KEY_POSTER_PATH);
 
@@ -38,8 +39,8 @@ public class DetailsActivity extends AppCompatActivity {
                 .with(this)
                 .load(imageUrl)
                 .placeholder(R.drawable.flicks_movie_placeholder)
-                .into(ivPosterDet);
+                .into(detailsActivityBinding.ivPosterDet);
 
-        rbMovie.setRating(rating);
+        detailsActivityBinding.rbMovie.setRating(rating);
     }
 }
